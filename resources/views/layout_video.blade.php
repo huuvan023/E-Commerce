96797c6658cc02ea7d142015789feedef485eac5
Van <huu.van.23tg@gmail.com>
2021-07-05 22:27:49 +0700

    Update website route url

--- resources/views/layout_video.blade.php
+++ resources/views/layout_video.blade.php
@@ -62,7 +62,7 @@
                 <div class="row">
                     <div class="col-sm-4">
                         <div class="logo pull-left">
-                            <a href="index.html"><img src="{{('/public/frontend/images/logo1.png')}}" alt="" /></a>
+                            <a href="/"><img src="{{('/public/frontend/images/logo1.png')}}" alt="" /></a>
                         </div>
                         <div class="btn-group pull-right">
                             <div class="btn-group">
