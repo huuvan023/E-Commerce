b40ca57f83e24d3f368b8d3be0a0264c92fe2495
Van <huu.van.23tg@gmail.com>
2021-07-06 13:02:13 +0700

    Update favicon url

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,7 +13,7 @@
     <link href="{{asset('public/frontend/css/style.css')}}" rel="stylesheet">
     <link href="{{asset('public/frontend/css/responsive.css')}}" rel="stylesheet">
      <link href="{{asset('public/frontend/css/sweetalert.css')}}" rel="stylesheet">
-    <link rel="shortcut icon" href="{{('public/frontend/images/favicon.ico')}}">
+    <link rel="shortcut icon" href="{{('/public/frontend/favicon/favicon.svg')}}">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
--- resources/views/layout_video.blade.php
+++ resources/views/layout_video.blade.php
@@ -13,7 +13,7 @@
     <link href="{{asset('/public/frontend/css/style.css')}}" rel="stylesheet">
     <link href="{{asset('/public/frontend/css/responsive.css')}}" rel="stylesheet">
      <link href="{{asset('/public/frontend/css/sweetalert.css')}}" rel="stylesheet">
-    <link rel="shortcut icon" href="{{('/public/frontend/images/favicon.ico')}}">
+    <link rel="shortcut icon" href="{{('/public/frontend/favicon/favicon.svg')}}">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
@@ -22,7 +22,7 @@
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-70NCR357BV"></script>
     <script>
-        window.dataLayer = window.dataLayer || [];      
+        window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
